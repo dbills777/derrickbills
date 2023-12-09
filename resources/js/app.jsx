e7b5c497169d650e0eac4eb4ab15b4a8f798d8b1
Derrick Bills <26029332+dbills777@users.noreply.github.com>
2023-12-09 15:23:18 -0700

    Add StrictMode to app render

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ThemeProvider } from "./Components/theme-provider";
+import { StrictMode } from "react";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
@@ -19,9 +20,11 @@ createInertiaApp({
         const root = createRoot(el);
 
         root.render(
-            <ThemeProvider>
-                <App {...props} />
-            </ThemeProvider>
+            <StrictMode>
+                <ThemeProvider>
+                    <App {...props} />
+                </ThemeProvider>
+            </StrictMode>
         );
     },
     progress: {
