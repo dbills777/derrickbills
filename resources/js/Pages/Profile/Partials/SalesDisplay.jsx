26eed9bcdc179c748a23b82d6283f8dce555cc57
Derrick Bills <26029332+dbills777@users.noreply.github.com>
2023-12-09 15:24:49 -0700

    Refactor sales display component

--- resources/js/Pages/Profile/Partials/SalesDisplay.jsx
+++ resources/js/Pages/Profile/Partials/SalesDisplay.jsx
@@ -5,22 +5,22 @@ export default function SalesDisplay() {
     return (
         <Card className="p-4 md:m-8">
             <h1 className="text-2xl font my-4 ">Monthy Sales</h1>
-            <div className="flex items-center">
+            <div className="flex items-center motion-safe:hover:scale-[1.01] transition-all duration-250 motion-safe:hover:text-primary ">
                 <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
                     <AvatarImage src="#" alt="Avatar" />
                     <AvatarFallback>OM</AvatarFallback>
                 </Avatar>
-                <div className="ml-4">
+                <div className="ml-4 ">
                     <p className="text-sm font-medium leading-none mt-4">
                         Olivia Martin
                     </p>
-                    <p className="text-sm text-muted-foreground">
+                    <p className="text-sm font-medium">
                         olivia.martin@email.com
                     </p>
                 </div>
                 <div className="ml-auto font-medium">+$1,999.00</div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center motion-safe:hover:scale-[1.01] transition-all duration-250 motion-safe:hover:text-primary ">
                 <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
                     <AvatarImage src="#" alt="Avatar" />
                     <AvatarFallback>JL</AvatarFallback>
@@ -29,13 +29,11 @@ export default function SalesDisplay() {
                     <p className="text-sm font-medium leading-none mt-4">
                         Jackson Lee
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                        jackson.lee@email.com
-                    </p>
+                    <p className="text-sm ">jackson.lee@email.com</p>
                 </div>
                 <div className="ml-auto font-medium">+$39.00</div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center motion-safe:hover:scale-[1.01] transition-all duration-250 motion-safe:hover:text-primary ">
                 <Avatar className="h-9 w-9">
                     <AvatarImage src="#" alt="Avatar" />
                     <AvatarFallback>IN</AvatarFallback>
@@ -44,13 +42,11 @@ export default function SalesDisplay() {
                     <p className="text-sm font-medium leading-none mt-4">
                         Isabella Nguyen
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                        isabella.nguyen@email.com
-                    </p>
+                    <p className="text-sm ">isabella.nguyen@email.com</p>
                 </div>
                 <div className="ml-auto font-medium">+$299.00</div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center motion-safe:hover:scale-[1.01] transition-all duration-250 motion-safe:hover:text-primary ">
                 <Avatar className="h-9 w-9">
                     <AvatarImage src="#" alt="Avatar" />
                     <AvatarFallback>WK</AvatarFallback>
@@ -59,13 +55,11 @@ export default function SalesDisplay() {
                     <p className="text-sm font-medium leading-none mt-4">
                         William Kim
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                        will@email.com
-                    </p>
+                    <p className="text-sm ">will@email.com</p>
                 </div>
                 <div className="ml-auto font-medium">+$99.00</div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center motion-safe:hover:scale-[1.01] transition-all duration-250 motion-safe:hover:text-primary ">
                 <Avatar className="h-9 w-9">
                     <AvatarImage src="#" alt="Avatar" />
                     <AvatarFallback>SD</AvatarFallback>
@@ -74,9 +68,7 @@ export default function SalesDisplay() {
                     <p className="text-sm font-medium leading-none mt-4">
                         Sofia Davis
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                        sofia.davis@email.com
-                    </p>
+                    <p className="text-sm ">sofia.davis@email.com</p>
                 </div>
                 <div className="ml-auto font-medium">+$39.00</div>
             </div>
