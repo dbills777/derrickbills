1a9bdfb6ccefaff2abb8ae25ec67b8fdd211e955
Derrick Bills <26029332+dbills777@users.noreply.github.com>
2023-12-10 11:39:42 -0700

    Remove purge plugin from Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import purge from "@erbelion/vite-plugin-laravel-purgecss";
 
 export default defineConfig({
     plugins: [
@@ -11,6 +10,5 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
-        purge(),
     ],
 });
