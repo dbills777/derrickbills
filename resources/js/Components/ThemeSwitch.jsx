bf40f3a01d4bb3c00481a72d447b15760a2ef803
Derrick Bills <26029332+dbills777@users.noreply.github.com>
2023-12-11 02:17:58 -0700

    Refactor theme switch component and set default theme to dark

--- resources/js/Components/ThemeSwitch.jsx
+++ resources/js/Components/ThemeSwitch.jsx
@@ -1,42 +1,20 @@
-import { useTheme } from "./theme-provider";
 import { MoonIcon } from "./Icons/MoonIcon";
 import { SunIcon } from "./Icons/SunIcon";
-import { VisuallyHidden, useSwitch } from "@nextui-org/react";
+import { useTheme } from "./theme-provider";
+import { Switch } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 const ThemeSwitch = (props) => {
     const { setTheme } = useTheme();
 
-    const {
-        Component,
-        slots,
-        isSelected,
-        getBaseProps,
-        getInputProps,
-        getWrapperProps,
-    } = useSwitch(props);
-
     return (
-        <> 
-            <Component {...getBaseProps()}>
-                <VisuallyHidden>
-                    <input {...getInputProps()} />
-                </VisuallyHidden>
-                <div
-                    {...getWrapperProps()}
-                    className={slots.wrapper({
-                        class: [
-                            "w-8 h-8",
-                            "flex items-center justify-center",
-                            "rounded-lg bg-default-100 hover:bg-default-200",
-                        ],
-                    })}
-                >
-                    {isSelected ? setTheme("light") : setTheme("dark")}
-                    {isSelected ? <SunIcon /> : <MoonIcon />}
-                </div>
-            </Component>
-            <p className="text-default-500 select-none">
-            </p>
+        <>
+            <Button variant="outline" onClick={() => setTheme("light")}>
+                Light
+            </Button>
+            <Button variant="outline" onClick={() => setTheme("dark")}>
+                Dark
+            </Button>
         </>
     );
 };
