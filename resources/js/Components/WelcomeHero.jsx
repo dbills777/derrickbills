53e2142065cd2181de66adbe40dd90e0e5655c32
Derrick Bills <dbills777@gmail.com>
2024-02-13 17:40:00 -0700

    Refactor WelcomeHero component and update content

--- resources/js/Components/WelcomeHero.jsx
+++ resources/js/Components/WelcomeHero.jsx
@@ -1,20 +1,11 @@
 import { motion } from "framer-motion";
+import { ButtonVariant } from "@/Components/Variants/ButtonVariant";
+
 const WelcomeHero = () => {
-    const imgURL = "http://localhost:8000/images/Circle.jpg";
-    const imgURL2 = "http://localhost:8000/images/AnimatedLockAndKey.gif";
-    const imgURL3 = "http://localhost:8000/images/text-gradient.avif";
-    function template({ rotate, x }) {
-        return `rotate(${rotate}) translateX(${x})`;
-    }
     return (
         <>
             <motion.div
                 className="hero min-h-screen"
-                style={{
-                    backgroundImage:
-                        // "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-                        "url(http://localhost:8000/images/Hero.png)",
-                }}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 10 }}
@@ -22,13 +13,19 @@ const WelcomeHero = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <h1 className="mb-5 text-5xl font-bold md:text-7xl">
+                            Hello There
+                        </h1>
                         <p className="mb-5">
-                            Provident cupiditate voluptatem et in. Quaerat
-                            fugiat ut assumenda excepturi exercitationem quasi.
-                            In deleniti eaque aut repudiandae et a id nisi.
+                            The application is built using the Laravel
+                            framework, powerful foundation for the application.
+                            This foundation leverages InertiaJS, ReactJS,
+                            Tailwind CSS.
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <p className="mb-5">
+                            It also uses Nextui, DaisyUI, and Framer Motion.
+                        </p>
+                        <ButtonVariant>NextUI Variant</ButtonVariant>
                     </div>
                 </div>
             </motion.div>
