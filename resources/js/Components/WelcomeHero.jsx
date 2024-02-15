6aef3d5d19f4518b431e5381dbb817caf4388543
Derrick Bills <dbills777@gmail.com>
2024-02-15 14:09:33 -0700

    Refactor WelcomeHero component and add new features

--- resources/js/Components/WelcomeHero.jsx
+++ resources/js/Components/WelcomeHero.jsx
@@ -1,35 +1,74 @@
+import { HeroCloudIcon } from "@/Components/Icons";
+import { Button, Tooltip } from "@nextui-org/react";
 import { motion } from "framer-motion";
-import { ButtonVariant } from "@/Components/Variants/ButtonVariant";
-
-const WelcomeHero = () => {
+export const WelcomeHero = () => {
     return (
-        <>
-            <motion.div
-                className="hero min-h-screen"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 10 }}
-            >
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold md:text-7xl">
-                            Hello There
-                        </h1>
-                        <p className="mb-5">
-                            The application is built using the Laravel
-                            framework, powerful foundation for the application.
-                            This foundation leverages InertiaJS, ReactJS,
-                            Tailwind CSS.
-                        </p>
-                        <p className="mb-5">
-                            It also uses Nextui, DaisyUI, and Framer Motion.
-                        </p>
-                        <ButtonVariant>NextUI Variant</ButtonVariant>
-                    </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 2 } }}
+            className="flex flex-col gap-2 py-20 items-center max-w-screen-xl
+                >
+                    mx-auto lg:flex-row"
+        >
+            <section className="flex flex-col space-y-8 ms-6 ">
+                <div>
+                    <h1 className="text-5xl md:text-6xl xl:text-7xl font-thin min-w-full">
+                        Welcome Hero Section
+                    </h1>
+                    <h2 className="text-2xl  mt-6">Subheading</h2>
                 </div>
-            </motion.div>
-        </>
+                <p className="sm:w-3/4">
+                    The application is built using the Laravel framework, which
+                    provides a unique and powerful foundation for the
+                    application. This foundation leverages InertiaJS, ReactJS,
+                    Tailwind CSS.
+                </p>
+                <p className=" sm:w-3/4">
+                    Using a monolithic architecture, allows for a single code
+                    base Creating a more efficient and maintainable application.
+                </p>
+                <div className="space-x-4">
+                    <Tooltip
+                        className="ms-3"
+                        placement="top"
+                        color="primary"
+                        content={
+                            <p className="text-tiny">
+                                {" "}
+                                This Button is a Tooltip{" "}
+                            </p>
+                        }
+                    >
+                        <Button radius="full" variant="ghost" color="primary">
+                            Get Started
+                        </Button>
+                    </Tooltip>
+                    <Tooltip
+                        className="ms-3 "
+                        offset={2}
+                        placement="right"
+                        color="primary"
+                        content={
+                            <>
+                                <p className="text-small font-bold">
+                                    Custimizable
+                                </p>
+                                <p className="text-tiny">
+                                    {" "}
+                                    This Button is a Tooltip{" "}
+                                </p>
+                            </>
+                        }
+                    >
+                        <Button variant="light" color="primary" radius="full">
+                            Learn More
+                        </Button>
+                    </Tooltip>
+                </div>
+            </section>
+            <section className="mx-auto lg:me-6">
+                <HeroCloudIcon height={400} width={400} />
+            </section>
+        </motion.div>
     );
 };
-export default WelcomeHero;
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,93 +1,17 @@
-import { motion } from "framer-motion";
 import Layout from "@/Layouts/Layout";
 import Footer from "@/Layouts/Footer";
 import { Head } from "@inertiajs/react";
-import { HeroCloudIcon } from "@/Components/Icons";
+import {WelcomeHero} from "@/Components/WelcomeHero";
 import { MotionIcons } from "@/Components/MotionIcons";
 import LandingPageCard from "@/Components/LandingPageCard";
-import { Button, Card, CardBody, CardHeader, Divider, Tooltip } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
 
 export default function Welcome({ auth }) {
     return (
         <>
             <Layout>
                 <Head title="Welcome" />
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1, transition: { duration: 2 } }}
-                    className="flex flex-col gap-2 items-center max-w-screen-xl
-                >
-                    mx-auto lg:flex-row"
-                >
-                    <section className="flex flex-col space-y-8 ms-6 ">
-                        <div>
-                            <h1 className="text-5xl font md:text-7xl">
-                                Welcome Hero Section
-                            </h1>
-                            <h2 className="text-2xl font-medium mt-6">Subheading</h2>
-                        </div>
-                        <p className="sm:w-3/4">
-                            The application is built using the Laravel
-                            framework, which provides a unique and powerful
-                            foundation for the application. This foundation
-                            leverages InertiaJS, ReactJS, Tailwind CSS.
-                        </p>
-                        <p className=" sm:w-3/4">
-                            Using a monolithic architecture, allows for a single
-                            code base Creating a more efficient and maintainable
-                            application.
-                        </p>
-                        <div className="space-x-4">
-                            <Tooltip
-                                className="ms-3"
-                                placement="top"
-                                color="primary"
-                                content={
-                                    <p className="text-tiny">
-                                        {" "}
-                                        This Button is a Tooltip{" "}
-                                    </p>
-                                }
-                            >
-                                <Button
-                                    radius="full"
-                                    variant="ghost"
-                                    color="primary"
-                                >
-                                    Get Started
-                                </Button>
-                            </Tooltip>
-                            <Tooltip
-                                className="ms-3 "
-                                offset={2}
-                                placement="right"
-                                color="primary"
-                                content={
-                                    <>
-                                        <p className="text-small font-bold">
-                                            Custimizable
-                                        </p>
-                                        <p className="text-tiny">
-                                            {" "}
-                                            This Button is a Tooltip{" "}
-                                        </p>
-                                    </>
-                                }
-                            >
-                                <Button
-                                    variant="light"
-                                    color="primary"
-                                    radius="full"
-                                >
-                                    Learn More
-                                </Button>
-                            </Tooltip>
-                        </div>
-                    </section>
-                    <section className="opacity-70 mx-auto lg:me-6">
-                        <HeroCloudIcon height={300} width={300} />
-                    </section>
-                </motion.div>
+                <WelcomeHero />
                 <div className="flex  mt-12 items-center mx-auto max-w-screen-xl ">
                     <section className="flex flex-col  space-y-8 ms-6">
                         <h2 className="text-5xl">Deployment Process</h2>
