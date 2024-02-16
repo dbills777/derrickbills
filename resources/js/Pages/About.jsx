057e212a553d9ca437a8c9abd7ff6349c0606386
Derrick Bills <dbills777@gmail.com>
2024-02-16 07:30:57 -0700

    Refactor About page layout and button styling

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -9,26 +9,26 @@ export default function About({ auth }) {
         <Layout>
             <Head title="About" />
             <WelcomeHero />
-            <div className="flex flex-col items-center justify-center ">
-                <h1 className="text-4xl text-center">About</h1>
-                <p className="text-center">
-                    About section stuff goes here. Lorem ipsum dolor sit amet
-                </p>
-            </div>
-            <div className="py-20 flex w-50 first:rounded-xl justify-around ">
-                <Button className="primary">Default</Button>
-                <Button color="primary">Primary</Button>
-                <Button color="secondary">Secondary</Button>
-                <Button color="greenie">Secondary</Button>
-                <Button color="success">Success</Button>
-                <Button color="warning">Warning</Button>
+            <section className="">
+                <div className="flex flex-col items-center justify-center ">
+                    <h1 className="text-4xl text-center font-thin my-6">About</h1>
+                    <p className="text-center">
+                        About section stuff goes here. Lorem ipsum dolor sit
+                        amet
+                    </p>
+                </div>
+                <div className="flex flex-col mt-20 items-center gap-3">
+                    <Button className="primary">Default</Button>
+                    <Button color="primary">Primary</Button>
+                    <Button color="secondary">Secondary</Button>
+                    <Button color="greenie">Secondary</Button>
+                    <Button color="success">Success</Button>
+                    <Button color="warning">Warning</Button>
 
-                <Button color="danger">Danger</Button>
-            </div>
-            <div className="flex justify-center ">
-                <p className="mx-3">Custom Variant Button</p>
-                <ButtonVariant color="ufo">UFO</ButtonVariant>
-            </div>
+                    <Button color="danger">Danger</Button>
+                    <ButtonVariant color="ufo">Custom Variant</ButtonVariant>
+                </div>
+            </section>
         </Layout>
     );
 }
