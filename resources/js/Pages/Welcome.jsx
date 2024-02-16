6ed4c450a63e6bb6cae25c27bfe5e11d28665003
Derrick Bills <dbills777@gmail.com>
2024-02-16 12:00:28 -0700

    Add Timeline component to Welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -5,6 +5,7 @@ import { WelcomeHero } from "@/Components/WelcomeHero";
 import { WelcomeSection } from "@/Components/WelcomeSection";
 import { MotionIcons } from "@/Components/MotionIcons";
 import { WelcomeCards } from "@/Components/WelcomeCards";
+import Timeline from "@/Components/Timeline";
 
 export default function Welcome({ auth }) {
     return (
@@ -12,6 +13,7 @@ export default function Welcome({ auth }) {
             <Layout>
                 <Head title="Welcome" />
                 <WelcomeHero />
+                <Timeline />
                 <WelcomeSection />
                 <WelcomeCards />
                 <MotionIcons />
