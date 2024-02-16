dc488563f9de19c283ef9337d2e8b1b90fb846e3
Derrick Bills <dbills777@gmail.com>
2024-02-16 12:15:51 -0700

    Add Timeline component to About page

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -4,6 +4,7 @@ import { Button } from "@nextui-org/react";
 import WelcomeHero from "@/Components/AboutHero";
 import { ButtonVariant } from "@/Components/Variants/ButtonVariant";
 import { Stats } from "@/Components/Stats";
+import Timeline from "@/Components/Timeline";
 
 export default function About({ auth }) {
     return (
@@ -13,27 +14,9 @@ export default function About({ auth }) {
             <section className="">
                 <div className="flex flex-col items-center justify-center py-10">
                     <Stats />
-                    <h1 className="text-4xl text-center font-thin my-6">
-                        About
-                    </h1>
-                    <p className="text-center">
-                        About section stuff goes here. Lorem ipsum dolor sit
-                        amet
-                    </p>
-
-                    <div className="flex flex-col mt-20 items-center gap-3">
-                        <Button className="primary">Default</Button>
-                        <Button color="primary">Primary</Button>
-                        <Button color="secondary">Secondary</Button>
-                        <Button color="greenie">Secondary</Button>
-                        <Button color="success">Success</Button>
-                        <Button color="warning">Warning</Button>
-
-                        <Button color="danger">Danger</Button>
-                        <ButtonVariant color="ufo">
-                            Custom Variant
-                        </ButtonVariant>
-                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center py-10">
+                    <Timeline />
                 </div>
             </section>
         </Layout>
