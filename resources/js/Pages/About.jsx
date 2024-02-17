44e417c88fde2a26a0a0137e143096fec9298b91
Derrick Bills <dbills777@gmail.com>
2024-02-17 16:33:38 -0700

    Add account balance and current balance stats

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -11,6 +11,30 @@ const About = ({ auth }) => {
             <AboutHero />
             <section className="">
                 <div className="flex flex-col items-center justify-center py-10">
+                    <div className="stats bg-primary text-primary-content">
+                        <div className="stat">
+                            <div className="stat-title">Account balance</div>
+                            <div className="stat-value">$89,400</div>
+                            <div className="stat-actions">
+                                <button className="btn btn-sm btn-success">
+                                    Add funds
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-title">Current balance</div>
+                            <div className="stat-value">$89,400</div>
+                            <div className="stat-actions">
+                                <button className="btn btn-sm m-1">
+                                    Withdrawal
+                                </button>
+                                <button className="btn btn-sm">deposit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-center py-10">
                     <Stats />
                 </div>
                 <div className="flex flex-col items-center justify-center py-10">
