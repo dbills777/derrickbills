be4911397f37fcb9815242f3bbd705b133fc4c2c
Derrick Bills <dbills777@gmail.com>
2024-02-17 15:31:57 -0700

    Refactor About page and related components

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return Inertia::render('About/About');
+    return Inertia::render('About');
 });
 
 Route::get('/products', function () {
