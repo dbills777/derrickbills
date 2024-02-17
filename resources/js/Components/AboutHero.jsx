d38917e290561371ef7203e1ce73c7ccd9320d56
Derrick Bills <dbills777@gmail.com>
2024-02-17 09:51:47 -0700

    Refactor AboutHero component Imports and Name

--- resources/js/Components/AboutHero.jsx
+++ resources/js/Components/AboutHero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { ButtonVariant } from "@/Components/Variants/ButtonVariant";
 
-export const AboutHero = () => {
+export const About = () => {
     return (
         <>
             <motion.div
