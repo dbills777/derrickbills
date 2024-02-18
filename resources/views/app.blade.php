13b19bd553156e790f5bdf897583b71a0a8267cc
Derrick Bills <dbills777@gmail.com>
2024-02-18 07:14:58 -0700

    Remove unused image import and code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx", "public/images/*"])
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
     <body class="antialiased bg-background text-foreground font-Xenon">
