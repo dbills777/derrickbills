1f32466468bb697c4e673e9cb6997a5d003f7199
Derrick Bills <dbills777@gmail.com>
2024-03-09 12:34:21 -0700

    Add missing line break in user route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Api\V1\CustomerController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
 
 Route::prefix('v1')->group(function () {
