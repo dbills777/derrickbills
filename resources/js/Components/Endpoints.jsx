c5118ee334060d54ef5309cd93421041ca46766d
Derrick Bills <dbills777@gmail.com>
2024-03-12 12:36:30 -0600

    Refactor EndPoints component to improve readability and add documentation

--- resources/js/Components/Endpoints.jsx
+++ resources/js/Components/Endpoints.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import { Link } from "@nextui-org/react";
 
 export default function EndPoints() {
-    const image = Math.floor(Math.random() * 1000);
     return (
         <>
-            <div className="flex flex-col w-full my-32 lg:flex-row my-64">
+            <section className="max-w-3xl mx-auto space-y-10 mt-32">
+                <h1 className="text-xl md:text-2xl xl:text-4xl font-thin text-center">
+                    Customer and Invoice endpoints
+                </h1>
+                <p>
+                    The following endpoints use laravel resource controllers to
+                    and collections to return data. The data is mutated using
+                    the controllers and returned as a collection in a json
+                    format.
+                </p>
+                <p>
+               All api controllers and resources are versioned allowing for updates and changes to be made without breaking existing api calls.
+                </p>
+            </section>
+            <div className="flex flex-col w-full my-32 lg:flex-row my-48">
                 <div className="grid flex-grow min-h-[400px] card bg-background rounded-box place-items-center">
                     <h1 className="text-3xl md:text-5xl xl:text-6xl font-thin ">
                         Customer endpoints
@@ -66,7 +79,7 @@ export default function EndPoints() {
             </div>
             <div className="grid flex-grow min-h-[800px] card bg-background rounded-box place-items-center">
                 <h1 className="text-3xl md:text-5xl xl:text-7xl font-thin ">
-                    Search 
+                    Search
                 </h1>
                 <p>all search params can be chained</p>
                 <p>
@@ -183,63 +196,6 @@ export default function EndPoints() {
                     </Link>
                 </p>
             </div>
-
-            {/* <section className="flex flex-col mx-auto  space-y-4 max-w-7xl">
-                <Link href="/api/v1/city" color="primary" underline>
-                    /api/v1/city
-                </Link>
-                <Link
-                    href="/api/v1/search?d_o_b=1980"
-                    color="primary"
-                    underline
-                >
-                    /api/v1/search?d_o_b=1980
-                </Link>
-                <br />
-                <Link
-                    href=" /api/v1/search?email=org"
-                    color="primary"
-                    underline
-                >
-                    /api/v1/search?email=org
-                </Link>
-                <br />
-                <br />
-                <Link href="/api/v1/name" color="primary" underline>
-                    /api/v1/name
-                </Link>
-                <br />
-                <Link href="/api/v1/phone" color="primary" underline>
-                    /api/v1/phone
-                </Link>
-                <br />
-                <Link href="/api/v1/customers/1" color="primary" underline>
-                    /api/v1/customers/1
-                </Link>
-                <br />
-                <Link href="/api/v1/invoices" color="primary" underline>
-                    /api/v1/invoices
-                </Link>
-                <br />
-                <Link href="/api/v1/invoices/1" color="primary" underline>
-                    /api/v1/invoices/1
-                </Link>
-                <br />
-                <Link href="/api/v1/search?name=john" color="primary" underline>
-                    /api/v1/search?name=jonn
-                </Link>
-                <br />
-                <Link href="/api/v1/search?email=org" color="primary" underline>
-                    /api/v1/search?email=org
-                </Link>
-                <br />
-                <Link
-                    href=" api/v1/search?d_o_b=01-01&name=hillary"
-                    color="success"
-                >
-                    api/v1/search?d_o_b=01-01&name=hillary
-                </Link>
-            </section> */}
         </>
     );
 }
