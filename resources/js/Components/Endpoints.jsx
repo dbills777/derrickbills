83b1ac6634d7bb623f608275fad7f23d60c6b7eb
Derrick Bills <dbills777@gmail.com>
2024-03-13 16:52:10 -0600

    Update Endpoints component

--- resources/js/Components/Endpoints.jsx
+++ resources/js/Components/Endpoints.jsx
@@ -20,7 +20,7 @@ export default function EndPoints() {
                     calls.
                 </p>
                 <ul className="menu menu-xs bg-background rounded-lg max-w-xs w-full">
-                    <li>
+                    {/* <li>
                         <a>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ export default function EndPoints() {
                             </svg>
                             resume.pdf
                         </a>
-                    </li>
+                    </li> */}
                     <li>
                         <details open>
                             <summary>
@@ -95,7 +95,7 @@ export default function EndPoints() {
                                                             d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                                                         />
                                                     </svg>
-                                                    Screenshot1.png
+                                                    Controllers
                                                 </a>
                                             </li>
                                             <li>
@@ -114,7 +114,7 @@ export default function EndPoints() {
                                                             d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                                                         />
                                                     </svg>
-                                                    Screenshot2.png
+                                                    Requests
                                                 </a>
                                             </li>
                                             <li>
@@ -134,7 +134,7 @@ export default function EndPoints() {
                                                                 d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
                                                             />
                                                         </svg>
-                                                        Requests
+                                                        Resources
                                                     </summary>
                                                     <ul>
                                                         <li>
@@ -153,7 +153,7 @@ export default function EndPoints() {
                                                                         d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                                                                     />
                                                                 </svg>
-                                                                Screenshot3.png
+                                                                Invoice Resource
                                                             </a>
                                                         </li>
                                                     </ul>
