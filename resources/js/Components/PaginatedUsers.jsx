a0c32650ba46cbc766f70888aadef61746781f1e
Derrick Bills <dbills777@gmail.com>
2024-03-13 20:22:00 -0600

    Refactor PaginatedUsers component

--- resources/js/Components/PaginatedUsers.jsx
+++ resources/js/Components/PaginatedUsers.jsx
@@ -27,44 +27,46 @@ export default function App({ users }) {
 
     return (
         <>
-            <Table
-                className="flex flex-col w-full max-w-7xl mx-auto mt-4"
-                aria-label="client-side pagination table example"
-                bottomContent={
-                    <div className="flex w-full justify-center">
-                        <Pagination
-                            showControls
-                            showShadow
-                            color="primary"
-                            page={page}
-                            total={pages}
-                            onChange={(page) => setPage(page)}
-                        />
-                    </div>
-                }
-            >
-                <TableHeader>
-                    <TableColumn key="name">name</TableColumn>
-                    <TableColumn key="email">email</TableColumn>
-                    <TableColumn key="created_at">created </TableColumn>
-                </TableHeader>
-
-                <TableBody
-                    items={items}
-                    className="flex justify-between w-full max-w-7xl"
+            <div className="min-h-screen">
+                <Table
+                    className="flex flex-col w-full max-w-7xl mx-auto mt-4"
+                    aria-label="client-side pagination table example"
+                    bottomContent={
+                        <div className="flex w-full justify-center">
+                            <Pagination
+                                showControls
+                                showShadow
+                                color="primary"
+                                page={page}
+                                total={pages}
+                                onChange={(page) => setPage(page)}
+                            />
+                        </div>
+                    }
                 >
-                    {(item) => (
-                        <TableRow key={item.email}>
-                            {(columnKey) => (
-                                <TableCell className="">
-                                    {getKeyValue(item, columnKey)}
-                                </TableCell>
-                            )}
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-            <UserList users={users} />
+                    <TableHeader>
+                        <TableColumn key="name">name</TableColumn>
+                        <TableColumn key="email">email</TableColumn>
+                        <TableColumn key="created_at">created </TableColumn>
+                    </TableHeader>
+
+                    <TableBody
+                        items={items}
+                        className="flex justify-between w-full max-w-7xl"
+                    >
+                        {(item) => (
+                            <TableRow key={item.email}>
+                                {(columnKey) => (
+                                    <TableCell className="">
+                                        {getKeyValue(item, columnKey)}
+                                    </TableCell>
+                                )}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+                {/* <UserList users={users} /> */}
+            </div>
         </>
     );
 }
