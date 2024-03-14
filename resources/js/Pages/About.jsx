13df313b073e9ea996e996e65ea5f07a21c01b1b
Derrick Bills <dbills777@gmail.com>
2024-03-13 20:18:50 -0600

    Remove unnecessary code in About.jsx

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -10,39 +10,11 @@ const About = ({ auth }) => {
         <Layout>
             <Head title="About" />
             <AboutHero />
-            <section className="">
-                <div className="flex flex-col items-center justify-center py-10">
-                    <div className="stats bg-primary text-primary-content">
-                        <div className="stat">
-                            <div className="stat-title">Account balance</div>
-                            <div className="stat-value">$89,400</div>
-                            <div className="stat-actions">
-                                <button className="btn btn-sm btn-success">
-                                    Add funds
-                                </button>
-                            </div>
-                        </div>
-
-                        <div className="stat">
-                            <div className="stat-title">Current balance</div>
-                            <div className="stat-value">$89,400</div>
-                            <div className="stat-actions">
-                                <button className="btn btn-sm m-1">
-                                    Withdrawal
-                                </button>
-                                <button className="btn btn-sm">deposit</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <section className="min-h-screen">
                 <div className="flex flex-col items-center justify-center py-10">
                     <Stats />
                 </div>
-                <WelcomeCards />
-
-                <div className="flex flex-col items-center justify-center py-10">
-                    <Timeline />
-                </div>
+                
             </section>
         </Layout>
     );
