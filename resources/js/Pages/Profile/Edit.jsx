ae18bdb84c183256bf52b9f82a067753c5b18205
Derrick Bills <dbills777@gmail.com>
2024-04-26 18:41:00 -0600

    Remove commented out code in Edit.jsx

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,7 +1,5 @@
 import Layout from "@/Layouts/Layout";
-// import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
 import { Head } from "@inertiajs/react";
-// import DeleteUserForm from "./Partials/DeleteUserForm";
 import UpdateProfileInformation from "./Partials/UpdateProfileInformationForm";
 
 export default function Edit({ auth, mustVerifyEmail, status }) {
@@ -21,14 +19,6 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                     />
                 </div>
             </section>
-
-            {/* <div className="max-w-7xl mx-auto p-4 my-6 sm:p-6 shadow sm:rounded-lg">
-                <UpdatePasswordForm />
-            </div>
-
-            <div className="max-w-7xl mx-auto p-4 sm:p-6 shadow sm:rounded-lg">
-                <DeleteUserForm />
-            </div> */}
         </Layout>
     );
 }
