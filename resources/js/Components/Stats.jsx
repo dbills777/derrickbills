655cee2c03a695cd8b3c41b06b843448f63e9e6a
Derrick Bills <dbills777@gmail.com>
2024-05-01 07:59:00 -0600

    Update styling and content in Stats component

--- resources/js/Components/Stats.jsx
+++ resources/js/Components/Stats.jsx
@@ -1,29 +1,8 @@
 export const Stats = () => {
     return (
-        <div className="stats shadow flex flex-col md:flex-row">
+        <div className="stats shadow flex flex-col stats-vertical md:flex-row md:stats-horizontal">
             <div className="stat">
-                <div className="stat-figure text-primary-400">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-9 h-9 stroke-current"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="1"
-                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                        ></path>
-                    </svg>
-                </div>
-                <div className="stat-title">Total Likes</div>
-                <div className="stat-value text-primary-400">25.6K</div>
-                <div className="stat-desc">21% more than last month</div>
-            </div>
-
-            <div className="stat">
-                <div className="stat-figure text-secondary-400">
+                <div className="stat-figure text-orange-500">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -39,12 +18,33 @@ export const Stats = () => {
                     </svg>
                 </div>
                 <div className="stat-title">Page Views</div>
-                <div className="stat-value text-secondary-400">2.6M</div>
+                <div className="stat-value text-orange-500">2.6M</div>
                 <div className="stat-desc">21% more than last month</div>
             </div>
 
             <div className="stat">
-                <div className="stat-figure text-success-400">
+                <div className="stat-figure text-primary">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-9 h-9 stroke-current"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="1"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                        ></path>
+                    </svg>
+                </div>
+                <div className="stat-title">Total Likes</div>
+                <div className="stat-value text-primary">25.6K</div>
+                <div className="stat-desc">21% more than last month</div>
+            </div>
+
+            <div className="stat">
+                <div className="stat-figure text-success">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -60,7 +60,7 @@ export const Stats = () => {
                     </svg>
                 </div>
                 <div className="stat-title ">Tasks done</div>
-                <div className="stat-value text-success-400">86%</div>
+                <div className="stat-value text-success">86%</div>
                 <div className="stat-desc">31 tasks remaining</div>
             </div>
         </div>
